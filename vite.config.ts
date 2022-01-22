04c47fa5353449c0422168dd0f60b857b4d3a1c8
Sun Jingchuan <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>
2022-01-22 20:40:23 +0800

    update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import type {ConfigEnv, UserConfig} from 'vite';
 import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 
@@ -15,6 +16,9 @@ export default defineConfig(({command, mode}: ConfigEnv): UserConfig => {
     base: VITE_BASE_URL,
     plugins: [
       vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
       Components({
         resolvers: [ElementPlusResolver()],
       }),
