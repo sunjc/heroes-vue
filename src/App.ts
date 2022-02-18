172fdc52aac687749a75b54b5805aa964f1731b5
Sun Jingchuan <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>
2022-02-18 16:29:54 +0800

    update dependencies

--- src/App.ts
+++ src/App.ts
@@ -1,21 +1,9 @@
 import {computed, defineComponent, ref} from 'vue'
 import {useI18n} from 'vue-i18n'
-import {ElCol, ElConfigProvider, ElContainer, ElHeader, ElMain, ElRadioButton, ElRadioGroup, ElRow} from 'element-plus'
 import enLocale from 'element-plus/lib/locale/lang/en'
 import zhLocale from 'element-plus/lib/locale/lang/zh-cn'
 
 export default defineComponent({
-  components: {
-    ElCol,
-    ElContainer,
-    ElHeader,
-    ElMain,
-    ElRadioButton,
-    ElRadioGroup,
-    ElRow,
-    [ElConfigProvider.name]: ElConfigProvider,
-  },
-
   setup() {
     const {t, d, locale} = useI18n()
     const supportLocales = [
